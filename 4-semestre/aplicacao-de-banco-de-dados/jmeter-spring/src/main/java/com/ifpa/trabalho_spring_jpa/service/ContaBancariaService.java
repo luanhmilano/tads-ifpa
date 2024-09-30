c51fd17addb9426fffa7bfdbea8d2b53f308ca89
Luan Henrique <luanhsantos14@gmail.com>
2024-09-29 21:58:25 -0300

    trabalho spring com jmeter

---
+++ 4-semestre/aplicacao-de-banco-de-dados/jmeter-spring/src/main/java/com/ifpa/trabalho_spring_jpa/service/ContaBancariaService.java
@@ -0,0 +1,59 @@
+package com.ifpa.trabalho_spring_jpa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ifpa.trabalho_spring_jpa.model.ContaBancaria;
+import com.ifpa.trabalho_spring_jpa.repository.ContaBancariaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContaBancariaService {
+    
+    @Autowired // explain this
+    private ContaBancariaRepository repository;
+
+    public List<ContaBancaria> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<ContaBancaria> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public ContaBancaria save(ContaBancaria contaBancaria) {
+        return repository.save(contaBancaria);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public synchronized void depositar(Long id, float valor) { 
+        Optional<ContaBancaria> conta = repository.findById(id); 
+        if (conta.isPresent()) { 
+            ContaBancaria contaBancaria = conta.get();
+            contaBancaria.setSaldo(contaBancaria.getSaldo() + valor);
+            repository.save(contaBancaria);
+        } else {
+            throw new RuntimeException("Conta não encontrada");
+        }
+    }
+
+    public synchronized void retirar(Long id, float valor) {
+        Optional<ContaBancaria> conta = repository.findById(id);
+        if (conta.isPresent()) {
+            ContaBancaria contaBancaria = conta.get();
+            if (contaBancaria.getSaldo() >= valor) {
+                contaBancaria.setSaldo(contaBancaria.getSaldo() - valor);
+                repository.save(contaBancaria);
+            } else {
+                throw new RuntimeException("Saldo insuficiente.");
+            }
+        } else {
+            throw new RuntimeException("Conta não encontrada.");
+        }
+    }
+}
